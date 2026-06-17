c740c37033e241b6c81465b9e0d7531fd4cb12ae
paulrogers5 <pr0969266@gmail.com>
2026-06-17 16:48:27 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -46,26 +46,19 @@ var init = function (window) {
         */
     function update() {
       // TODO 4 : Update the position of each circle using physikz.updatePosition()
-      physikz.updatePosition(circles[0]);
-       physikz.updatePosition(circles[1]);
-        physikz.updatePosition(circles[2]);
-         physikz.updatePosition(circles[3]);
-          physikz.updatePosition(circles[4]);
+      
       
       
       // TODO 5 : Call game.checkCirclePosition() on your circles
-      game.checkCirclePosition(circles[0])
-      game.checkCirclePosition(circles[1])
-      game.checkCirclePosition(circles[2])
-      game.checkCirclePosition(circles[3])
-      game.checkCirclePosition(circles[4])
+    
       // TODO 8 / TODO 9 : Iterate over the array
-    }
+    
 
     for (var i = 0; i < circles.length; i++) {
-  physikz.updatePosition
+  physikz.updatePosition(circles[i])
+  game.checkCirclePosition(circles[i])
 }
-
+  }
     /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
