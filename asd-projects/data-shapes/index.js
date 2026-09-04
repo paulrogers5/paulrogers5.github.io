7752ec429eae78aa102add8bb8b2b0bf8c2ca822
paulrogers5 <pr0969266@gmail.com>
2026-09-04 00:44:31 +0000

    saving data shapes progress

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -35,12 +35,36 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
 
   // TODO 1: create a new shape object and add it to the array
-  
+  var shape = {
+    "color": "blue",
+    'shape': 'circle',
+    'repeat': 3
+  }
+  dataShapes.push(shape)
 
   // TODO 2: add a new property to all data shapes
+  for (var i = 0; i < dataShapes.length; i++){
+    var currentShape = dataShapes[i]
+    if (currentShape.color === "red"){
+      currentShape.goodBehavior = "bounce"
+    }
+    else if (currentShape.color === "blue"){
+      currentShape.goodBehavior = "blink"
+    }
+    else{
+      currentShape.goodBehavior = "spin"
+    }
+  }
+  
   
 
   // TODO 3-a: add a function that handles the static display type
+  function handleStatic(data){
+    setBackgroundWithObject(data)
+    animationDetails.displayType = 1
+  
+  }
+  handleStatic(currentShape)
   
 
   // TODO 4-a: add a function that handles the good display type
